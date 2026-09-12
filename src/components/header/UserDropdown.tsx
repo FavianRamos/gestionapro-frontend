c341ddf567c2e57d9930282173ae9d27b61a1c27
Favian Ramos <favian29072003@gmail.com>
2026-09-11 21:42:59 -0500

    Fix unused imports in UserDropdown

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import { DropdownItem } from "../ui/dropdown/DropdownItem";
 import { Dropdown } from "../ui/dropdown/Dropdown";
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 function decodeRoleFromToken(token: string): string {
   try {
